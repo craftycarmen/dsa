101a1e0e06b4937b02f75f456b8e2252fa51c17a
Carmen <carmenshiu07@gmail.com>
2024-08-02 17:11:10 -0700

    lc-3-solution_2

--- 3-longest_substring_without_repeating_characters.py
+++ 3-longest_substring_without_repeating_characters.py
@@ -23,31 +23,49 @@
 # s consists of English letters, digits, symbols and spaces.
 
 def lengthOfLongestSubString(self, s):
+    # left, right = 0, 0
+    # # set left and right pointers that define the curr window of the substring
+
+    # max_length = 0
+    # # set max_length to keep track of the lonest substring
+
+    # window = set()
+    # # create a set that stores chars currently in substring window, ensuring all chars are unique
+
+    # while right < len(s):
+    #     # while the right pointer is within the bounds of the string
+        
+    #     window.discard(s[left])
+    #     left += 1
+
+    #     # this slides the left boundary of the window to the right:
+    #         # it removes the char at the left pointer from the window (window.discoard(s[left])
+    #         # increment left
+
+    #     max_length = max(max_length, right - left + 1)
+    #     # update max_length to the curr window if it is larger than the prev max length
+    #     # window length is calculated as right - left + 1
+
+    #     window.add(s[right])
+    #     right += 1
+    #     # add the character at the right pointer to the window set and move the right pointer to the right
+    
+    # return max_length
+
     left, right = 0, 0
-    # set left and right pointers that define the curr window of the substring
-
     max_length = 0
-    # set max_length to keep track of the lonest substring
-
     window = set()
-    # create a set that stores chars currently in substring window, ensuring all chars are unique
 
     while right < len(s):
-        # while the right pointer is within the bounds of the string
+        if s[right] not in window:
+            window.add(s[right])
+            max_length = max(max_length, right - left + 1)
+            right += 1
+        else:
+            window.discard(s[left])
+            left += 1
         
-        window.discard(s[left])
-        left += 1
-
-        # this slides the left boundary of the window to the right:
-            # it removes the char at the left pointer from the window (window.discoard(s[left])
-            # increment left
-
-        max_length = max(max_length, right - left + 1)
-        # update max_length to the curr window if it is larger than the prev max length
-        # window length is calculated as right - left + 1
-
-        window.add(s[right])
-        right += 1
-        # add the character at the right pointer to the window set and move the right pointer to the right
-    
     return max_length
+
+
+# the above version would make the left pointer only move when a dupe char is found
