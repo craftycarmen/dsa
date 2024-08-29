546f5377799810c69763d3a7f68b304a175e74d2
Carmen <carmenshiu07@gmail.com>
2024-08-29 16:39:03 -0700

    lc-144

---
+++ 144-binary_tree_preorder_traversal.py
@@ -0,0 +1,35 @@
+# 144. Binary Tree Preorder Traversal
+
+# Given the root of a binary tree, return the preorder traversal of its nodes' values.
+
+# Example 1:
+# Input: root = [1,null,2,3]
+# Output: [1,2,3]
+
+# Example 2:
+# Input: root = [1,2,3,4,5,null,8,null,null,6,7,9]
+# Output: [1,2,4,5,6,7,3,8,9]
+
+# Example 3:
+# Input: root = []
+# Output: []
+
+# Example 4:
+# Input: root = [1]
+# Output: [1]
+
+# Constraints:
+# The number of nodes in the tree is in the range [0, 100].
+# -100 <= Node.val <= 100
+ 
+# Follow up: Recursive solution is trivial, could you do it iteratively?
+
+# preorder is 1) process node; 2) traverse left tree; 3) traverse right tree
+
+def preorderTraversal(self, root):
+    if not root: return []
+
+    left = self.preorderTraversal(root.left)
+    right = self.preorderTraversal(root.right)
+
+    return [root.val] + left + right
