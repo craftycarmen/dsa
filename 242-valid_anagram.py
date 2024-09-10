e14db2ce1199979155784a01b0d1f477a844cfff
Carmen <carmenshiu07@gmail.com>
2024-09-09 21:33:42 -0700

    lc-242py

---
+++ 242-valid_anagram.py
@@ -0,0 +1,36 @@
+# 242. Valid Anagram
+# Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+# An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+# Example 1:
+# Input: s = "anagram", t = "nagaram"
+# Output: true
+
+# Example 2:
+# Input: s = "rat", t = "car"
+# Output: false
+
+# Constraints:
+# 1 <= s.length, t.length <= 5 * 104
+# s and t consist of lowercase English letters.
+
+# Follow up: What if the inputs contain Unicode characters? How would you adapt your solution to such a case?
+
+def isAnagram(self, s, t):
+    if len(s) != len(t):
+        return False
+    
+    countS, countT = {}, {}
+
+    for i in range(len(s)):
+        countS[s[i]] = 1 + countS.get(s[i], 0)
+        # need to do .get in python or else it'll throw an error if key doesn't already exist in hashhmap -- second paramater makes it return 0
+
+        countT[t[i]] = 1 + countT.get(t[i], 0)
+    
+    for c in countS:
+        if countS[c] != countT.get(c, 0):
+            return False
+        
+    return True
