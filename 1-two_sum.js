3f6392bcdcdde3d5ecfc4c33579051aa7ddead4d
Carmen <carmenshiu07@gmail.com>
2024-09-11 20:19:03 -0700

    lc-1

--- 1-two_sum.js
+++ 1-two_sum.js
@@ -7,24 +7,20 @@
 // You can return the answer in any order.
 
 
-
 // Example 1:
-
 // Input: nums = [2,7,11,15], target = 9
 // Output: [0,1]
 // Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
-// Example 2:
 
+// Example 2:
 // Input: nums = [3,2,4], target = 6
 // Output: [1,2]
-// Example 3:
 
+// Example 3:
 // Input: nums = [3,3], target = 6
 // Output: [0,1]
 
-
 // Constraints:
-
 // 2 <= nums.length <= 104
 // -109 <= nums[i] <= 109
 // -109 <= target <= 109
@@ -32,6 +28,17 @@
 
 
 // Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
+
+
+
+// create an empty object to keep track of which numbers were seen
+// iterate over each index 'i' of the nums array
+// get the curr number
+// calculate the complement that would sum with num to reach target
+// if the complement is in seen, then return the indices of the complement number and curr number
+// otherwise, add the curr number and its index to seen
+// if no pair found, return false
+
 
 var twoSum = function (nums, target) {
     const seen = {};
