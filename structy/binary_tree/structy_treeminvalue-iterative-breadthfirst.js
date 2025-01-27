81135475a048de8705986966581974f7da8a0024
Carmen <carmenshiu07@gmail.com>
2025-01-27 12:06:05 -0800

    s-treeminval-iter-bf

---
+++ structy/binary_tree/structy_treeminvalue-iterative-breadthfirst.js
@@ -0,0 +1,123 @@
+// tree min value
+// Write a function, treeMinValue, that takes in the root of a binary tree that contains number values.The function should return the minimum value within the tree.
+
+// You may assume that the input tree is non - empty.
+
+// *** ITERATIVE - BREADTH FIRST *** //
+// 1. create a let variable called smallest initialized to positive infinity
+// 2. create a constant variable called queue with the root passed into an array
+// 3. while there is a queue:
+//      a. create a constant variable called current that removes the front element of queue via .shift()
+//      b. if current's value is less than smallest, replace smallest with current's value
+//      c. if current has a left child, push current's left child to stack
+//      d. if current has a right child, push current's right child to stack
+// 4. return smallest
+
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+const treeMinValue = (root) => {
+    let smallest = Infinity;
+    const queue = [root];
+
+    while (queue.length > 0) {
+        const current = queue.shift();
+
+        if (current.val < smallest) smallest = current.val;
+
+        if (current.left) queue.push(current.left);
+        if (current.right) queue.push(current.right);
+    }
+
+    return smallest;
+}
+
+// n = number of nodes
+// time complexity: O(n2) technically because of .shift() --> if you remove the front element of an array in JS, you will have to shift all the other elements over one position
+// space complexity: O(n)
+
+// // *** TEST 00 *** //
+// const a = new Node(3);
+// const b = new Node(11);
+// const c = new Node(4);
+// const d = new Node(4);
+// const e = new Node(-2);
+// const f = new Node(1);
+
+// a.left = b;
+// a.right = c;
+// b.left = d;
+// b.right = e;
+// c.right = f;
+
+// //       3
+// //    /    \
+// //   11     4
+// //  / \      \
+// // 4   -2     1
+
+// console.log(treeMinValue(a)); // -> -2
+
+
+// // *** TEST 01 *** //
+const a = new Node(5);
+const b = new Node(11);
+const c = new Node(3);
+const d = new Node(4);
+const e = new Node(14);
+const f = new Node(12);
+
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
+c.right = f;
+
+//       5
+//    /    \
+//   11     3
+//  / \      \
+// 4   14     12
+
+console.log(treeMinValue(a)); // -> 3
+
+
+// // *** TEST 02 *** //
+// const a = new Node(-1);
+// const b = new Node(-6);
+// const c = new Node(-5);
+// const d = new Node(-3);
+// const e = new Node(-4);
+// const f = new Node(-13);
+// const g = new Node(-2);
+// const h = new Node(-2);
+
+// a.left = b;
+// a.right = c;
+// b.left = d;
+// b.right = e;
+// c.right = f;
+// e.left = g;
+// f.right = h;
+
+// //        -1
+// //      /   \
+// //    -6    -5
+// //   /  \     \
+// // -3   -4   -13
+// //     /       \
+// //    -2       -2
+
+// console.log(treeMinValue(a)); // -> -13
+
+
+// // *** TEST 03 *** //
+// const a = new Node(42);
+
+// //        42
+
+// console.log(treeMinValue(a)); // -> 42
