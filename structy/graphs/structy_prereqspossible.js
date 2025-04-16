89a9dd232194cbf4b1b53d8dd97d98fb28903dda
Carmen <carmenshiu07@gmail.com>
2025-04-16 15:54:23 -0700

    s-preqsposs pseudo

--- structy/graphs/structy_prereqspossible.js
+++ structy/graphs/structy_prereqspossible.js
@@ -1,6 +1,31 @@
 // prereqs possible
 // Write a function, prereqsPossible, that takes in a number of courses(n) and prerequisites as arguments.Courses have ids ranging from 0 through n - 1. A single prerequisite of[A, B] means that course A must be taken before course B.The function should return a boolean indicating whether or not it is possible to complete all courses.
 
+// A. in the man function:
+//      1. save the call of the buildGraph helper function, taking in numCourses and prereqs, as a constant variable called graph
+//      2. initialize a new set called visiting
+//      3. initialize a new set called visited
+//      4. iterate through node in graph:
+//              a. if the call of hasCycle, taking in graph, node, visiting, and visited, returns true, return false
+//      5. return true
+// B. declare a hasCycle helper function, taking in graph, node, visiting, and visited
+//      1. if visited set has node, return false - we are visited this node.
+//      2. if visiting set has node, return true - we are in the midst of a cycle.
+//      3. add node to visiting set
+//      4. iterate through neighbor of graph at node:
+//              a. if the recursive call of hasCycle, passing in graph, neighbor, visiting, and visited returns true, return true
+//      5. delete node from visiting set
+//      6. add node to visited set
+//      7. return false
+// C. declare a buildGraph helper function that takes in numCourses and prereqs to turn the preres into an adjacency list:
+//      1. initialize an empty object called graph
+//      2. iterate through the course:
+//              a. set graph at course to an empty array
+//      3. iterate through prereq of prereqs:
+//              a. unpack courseA and courseB from prereq
+//              b. add courseB as a string to the neighbors of courseA in the graph
+//      4. return graph
+
 
 // // *** TEST 0 *** //
 // const numCourses = 6;
