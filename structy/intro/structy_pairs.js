4da3ff4d9824cf7970397a73f85ec24fa3454fe2
Carmen <carmenshiu07@gmail.com>
2025-04-24 11:29:43 -0700

    s-pairs prob

---
+++ structy/intro/structy_pairs.js
@@ -0,0 +1,50 @@
+// pairs
+// Write a function, pairs, that takes in an array as an argument.The function should return an array contain all unique pairs of elements.
+
+// You may return the pairs in any order and the order of elements within a single pair does not matter.
+
+// You can assume that the input array contains unique elements.
+
+
+//     // *** TEST 00 *** //
+//     console.log(pairs(["a", "b", "c"])); // ->
+//     // [
+//     //    ["a", "b"],
+//     //    ["a", "c"],
+//     //    ["b", "c"]
+//     // ]
+    
+
+
+//     // *** TEST 01 *** //
+//     console.log(pairs(["a", "b", "c", "d"])); // ->
+// // [
+// //    ["a", "b"],
+// //    ["a", "c"],
+// //    ["a", "d"],
+// //    ["b", "c"],
+// //    ["b", "d"],
+// //    ["c", "d"]
+// // ]
+
+
+
+// // *** TEST 02 *** //
+// console.log(pairs(["cherry", "cranberry", "banana", "blueberry", "lime", "papaya"])); // ->
+// // [ 
+// //   [ "cherry", "cranberry" ], 
+// //   [ "cherry", "banana" ], 
+// //   [ "cherry", "blueberry" ], 
+// //   [ "cherry", "lime" ], 
+// //   [ "cherry", "papaya" ], 
+// //   [ "cranberry", "banana" ], 
+// //   [ "cranberry", "blueberry" ], 
+// //   [ "cranberry", "lime" ], 
+// //   [ "cranberry", "papaya" ], 
+// //   [ "banana", "blueberry" ], 
+// //   [ "banana", "lime" ], 
+// //   [ "banana", "papaya" ], 
+// //   [ "blueberry", "lime" ], 
+// //   [ "blueberry", "papaya" ], 
+// //   [ "lime", "papaya" ] 
+// // ] 
