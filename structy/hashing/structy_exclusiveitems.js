3ee51bfe3acaf05ef8b228a40b9a14736a21c5cf
Carmen <carmenshiu07@gmail.com>
2025-04-28 10:58:05 -0700

    s-exclusiveitems prob

---
+++ structy/hashing/structy_exclusiveitems.js
@@ -0,0 +1,25 @@
+// exclusive items
+// Write a function, exclusiveItems, that takes in two arrays, a, b, as arguments.The function should return a new array containing elements that are in either array but not both arrays.
+
+// You may assume that each input array does not contain duplicate elements.
+
+
+
+// // ** TEST 00 ** //
+//     console.log(exclusiveItems([4, 2, 1, 6], [3, 6, 9, 2, 10])); // -> [4,1,3,9,10]
+
+
+// // ** TEST 01 ** //
+// console.log(exclusiveItems([2, 4, 6], [4, 2])); // -> [6]
+
+
+// // ** TEST 02 ** //
+// console.log(exclusiveItems([4, 2, 1], [1, 2, 4, 6])); // -> [6]
+
+
+// // ** TEST 03 ** //
+// console.log(exclusiveItems([0, 1, 2], [10, 11])); // -> [0,1,2,10,11]
+
+
+// // ** TEST 04 ** //
+// console.log(exclusiveItems([0, 1, 2], [10, 11])); // -> [0,1,2,10,11]
