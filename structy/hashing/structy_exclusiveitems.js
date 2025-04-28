b252bb18324cef91548cc1d2a4efbc433a047cd5
Carmen <carmenshiu07@gmail.com>
2025-04-28 11:01:59 -0700

    s-exclusiveitems solution

--- structy/hashing/structy_exclusiveitems.js
+++ structy/hashing/structy_exclusiveitems.js
@@ -3,10 +3,33 @@
 
 // You may assume that each input array does not contain duplicate elements.
 
+// 1. convert "a" array into a set
+// 2. convert "b" array into a set
+// 3. initialize an empty array to store results
+// 4. iterate through "a" array:
+//      a. if element is NOT in set A, add element to results
+// 5. iterate through "b" array:
+//      b. if element is NOT in set B, add element to results
+// 6. return results
 
+const exclusiveItems = (a, b) => {
+    const setA = new Set(a);
+    const setB = new Set(b);
+    const res = [];
+
+    for (let ele of a) {
+        if (!setB.has(ele)) res.push(ele);
+    }
+
+    for (let ele of b) {
+        if (!setA.has(ele)) res.push(ele);
+    }
+
+    return res;
+}
 
 // // ** TEST 00 ** //
-//     console.log(exclusiveItems([4, 2, 1, 6], [3, 6, 9, 2, 10])); // -> [4,1,3,9,10]
+// console.log(exclusiveItems([4, 2, 1, 6], [3, 6, 9, 2, 10])); // -> [4,1,3,9,10]
 
 
 // // ** TEST 01 ** //
