451a6a0cfac350f2c9d914f00b5b51e726d5cf25
Carmen <carmenshiu07@gmail.com>
2025-04-29 10:02:19 -0700

    s-allunique solution

--- structy/hashing/structy_allunique.js
+++ structy/hashing/structy_allunique.js
@@ -1,22 +1,31 @@
 // all unique
 // Write a function, allUnique, that takes in an array.The function should return a boolean indicating whether or not the array contains unique items.
 
+// 1. turn the array into a set
+// 2. return whether length of array is the same as the size of set
 
-// // *** TEST 00 *** //
-// console.log(allUnique(["q", "r", "s", "a"])); // -> true
+const allUnique = (items) => {
+    const uniqueItems = new Set(items);
 
-
-// // *** TEST 01 *** //
-// console.log(allUnique(["q", "r", "s", "a", "r", "z"])); // -> false
-
-
-// // *** TEST 02 *** //
-// console.log(allUnique(["red", "blue", "yellow", "green", "orange"])); // -> true
+    return items.length === uniqueItems.size;
+}
 
 
-// // *** TEST 03 *** //
-// console.log(allUnique(["cat", "cat", "dog"])); // -> false
+// *** TEST 00 *** //
+console.log(allUnique(["q", "r", "s", "a"])); // -> true
 
 
-// // *** TEST 04 *** //
-// console.log(allUnique(["a", "u", "t", "u", "m", "n"])); // -> false
+// *** TEST 01 *** //
+console.log(allUnique(["q", "r", "s", "a", "r", "z"])); // -> false
+
+
+// *** TEST 02 *** //
+console.log(allUnique(["red", "blue", "yellow", "green", "orange"])); // -> true
+
+
+// *** TEST 03 *** //
+console.log(allUnique(["cat", "cat", "dog"])); // -> false
+
+
+// *** TEST 04 *** //
+console.log(allUnique(["a", "u", "t", "u", "m", "n"])); // -> false
